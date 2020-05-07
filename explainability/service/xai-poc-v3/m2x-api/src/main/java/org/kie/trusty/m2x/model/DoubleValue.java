182fe8f51abaabb7fafb4c947c374d1b1a13c11e
Tommaso Teofili <tteofili@redhat.com>
2020-05-07 10:29:34 +0200

    FAI-134 - adding missing value extensions

---
+++ explainability/service/xai-poc-v3/m2x-api/src/main/java/org/kie/trusty/m2x/model/DoubleValue.java
@@ -0,0 +1,8 @@
+package org.kie.trusty.m2x.model;
+
+public class DoubleValue extends Value<Double> {
+
+    public DoubleValue(Double underlyingObject) {
+        super(underlyingObject);
+    }
+}
---
+++ explainability/service/xai-poc-v3/m2x-api/src/main/java/org/kie/trusty/m2x/model/StringValue.java
@@ -0,0 +1,8 @@
+package org.kie.trusty.m2x.model;
+
+public class StringValue extends Value<String> {
+
+    public StringValue(String underlyingObject) {
+        super(underlyingObject);
+    }
+}
