4725f6fc48a4bad8f8ee21754f799aa0e0defcfd
roberto emanuel <kelvah@gmail.com>
2020-05-12 14:07:07 +0200

    [FAI-141] Added mortgage distribution chart

--- front-end-poc/src/AuditOverview/ModelOverview.tsx
+++ front-end-poc/src/AuditOverview/ModelOverview.tsx
@@ -1,44 +1,45 @@
-import React from 'react';
-import {Card, CardBody, CardHeader, Grid, GridItem, Title} from "@patternfly/react-core";
-import RecommendationDistributionChart from "../ModelLookup/RecommendationDistributionChart";
+import React from "react";
+import { Card, CardBody, CardHeader, Grid, GridItem, Title } from "@patternfly/react-core";
+import MortgageDistribution from "../ModelLookup/MortgageDistribution";
 
 function ModelOverview() {
-    return (
-        <Card>
-            <CardHeader>
-                <Title headingLevel="h3" size="2xl">
-                    Model Lookup
-                </Title>
-            </CardHeader>
-            <CardBody>
-                <Grid gutter="md" className={"data"}>
-                    <GridItem span={6}>
-                        <label className={"data__label"}>Version</label>
-                        <span>v5.0</span>
-                    </GridItem>
-                    <GridItem span={6}>
-                        <label className={"data__label"}>Authored on</label>
-                        <span>20/01/2020</span>
-                    </GridItem>
-                    <GridItem span={6}>
-                        <label className={"data__label"}>Released By</label>
-                        <span>Anouk Aimée</span>
-                    </GridItem>
-                    <GridItem span={6}>
-                        <label className={"data__label"}># of Decisions Produced</label>
-                        <span>1,523</span>
-                    </GridItem>
-                    <GridItem span={12}>
-                        <Title headingLevel="h5" size="lg">
-                            Decision Distribution (last 90 days)
-                        </Title>
-                        {/* <DecisionDistributionChart /> */ }
-                        <RecommendationDistributionChart />
-                    </GridItem>
-                </Grid>
-            </CardBody>
-        </Card>
-    );
+  return (
+    <Card>
+      <CardHeader>
+        <Title headingLevel="h3" size="2xl">
+          Model Lookup
+        </Title>
+      </CardHeader>
+      <CardBody>
+        <Grid gutter="md" className={"data"}>
+          <GridItem span={6}>
+            <label className={"data__label"}>Version</label>
+            <span>v5.0</span>
+          </GridItem>
+          <GridItem span={6}>
+            <label className={"data__label"}>Authored on</label>
+            <span>20/01/2020</span>
+          </GridItem>
+          <GridItem span={6}>
+            <label className={"data__label"}>Released By</label>
+            <span>Anouk Aimée</span>
+          </GridItem>
+          <GridItem span={6}>
+            <label className={"data__label"}># of Decisions Produced</label>
+            <span>1,523</span>
+          </GridItem>
+          <GridItem span={12}>
+            <Title headingLevel="h5" size="lg">
+              Decision Distribution (last 60 days)
+            </Title>
+            {/* <DecisionDistributionChart /> */}
+            {/* <RecommendationDistributionChart /> */}
+            <MortgageDistribution />
+          </GridItem>
+        </Grid>
+      </CardBody>
+    </Card>
+  );
 }
 
 export default ModelOverview;
