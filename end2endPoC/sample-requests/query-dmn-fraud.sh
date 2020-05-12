721e6cd4e8aeca90e44e83d02579000c51e659b3
jake <jacopo.r00ta@gmail.com>
2020-05-12 19:55:23 +0200

    fix type handling

--- end2endPoC/sample-requests/query-dmn-fraud.sh
+++ end2endPoC/sample-requests/query-dmn-fraud.sh
@@ -1,1 +1,1 @@
-curl --location --request POST 'http://localhost:1337/fraud-scoring' --header 'Content-Type: application/json' -d "{  \"Transactions\" : [{\"tRiskScore\" : 1, \"tCardType\" : \"Debit\", \"tAuthCode\" : \"Authorized\", \"tLocation\" : \"Local\"}]}"
+curl --location --request POST 'http://localhost:1337/fraud-scoring' --header 'Content-Type: application/json' -d "{  \"Transactions\" : [{\"Card Type\" : \"Debit\", \"Location\" : \"Local\", \"Amount\" : 10000, \"Auth Code\" : \"Authorized\"}]}"
