76282bbe29dcc07fa58f2aac0d6d3177f10b350b
jake <jacopo.r00ta@gmail.com>
2020-05-12 16:03:38 +0200

    fix integration tests

--- end2endPoC/sample-requests/query-dmn-fraud.sh
+++ end2endPoC/sample-requests/query-dmn-fraud.sh
@@ -1,1 +1,1 @@
-curl --location --request POST 'http://localhost:8080/fraud-scoring' --header 'Content-Type: application/json' -d "{  \"Transactions\" : [{\"tRiskScore\" : 1, \"tCardType\" : \"Debit\", \"tAuthCode\" : \"Authorized\", \"tLocation\" : \"Local\"}]}"
+curl --location --request POST 'http://localhost:1337/fraud-scoring' --header 'Content-Type: application/json' -d "{  \"Transactions\" : [{\"tRiskScore\" : 1, \"tCardType\" : \"Debit\", \"tAuthCode\" : \"Authorized\", \"tLocation\" : \"Local\"}]}"
