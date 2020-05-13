16c5e20639475e3f95216847c3b192d584e6e564
Michael Anstis <manstis@redhat.com>
2020-05-13 15:43:02 +0100

    Add support for mocked viewer for end2end models based on modelName

--- front-end-poc/src/ModelLookup/ModelDiagram.tsx
+++ front-end-poc/src/ModelLookup/ModelDiagram.tsx
@@ -5,6 +5,11 @@ import { LinearRegressionViewer } from "./pmml/LinearRegressionViewer";
 const DMN1_2: string = "http://www.omg.org/spec/DMN/20151101/dmn.xsd";
 const PMML4_4: string = "http://www.dmg.org/PMML-4_4";
 
+const model1Url: string = "https://gist.githubusercontent.com/r00ta/c5077ac1f12746e356e1d4f03620ee05/raw/a3d2a865398547a18010e78dd4c1b0d76cc85d99/myMortgage.dmn";
+const defaultModelUrl: string = "https://raw.githubusercontent.com/kiegroup/kogito-tooling/master/packages/online-editor/static/samples/sample.dmn";
+
+let models: Map<string, string> = new Map([["myMortgage", model1Url]]);
+
 interface Props {
   model: IExecutionModelResponse;
 }
@@ -14,8 +19,8 @@ function makeUnknownModel(): JSX.Element {
 }
 
 function makeDMNEditor(model: IExecutionModelResponse): JSX.Element {
-  const modelUrl: string = "https://raw.githubusercontent.com/kiegroup/kogito-tooling/master/packages/online-editor/static/samples/sample.dmn#/editor/dmn";
-  const editorUrl = `https://kiegroup.github.io/kogito-online/?file=${modelUrl}`;
+  const modelUrl: string = models.get(model.name) ?? defaultModelUrl;
+  const editorUrl = `https://kiegroup.github.io/kogito-online/?file=${modelUrl}#/editor/dmn`;
   const kogitoIframe = () => {
     return { __html: `<iframe src=${editorUrl}"></iframe>` };
   };
