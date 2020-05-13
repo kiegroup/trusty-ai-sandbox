c607b729076495e723cf8496da879454fdffab68
jake <jacopo.r00ta@gmail.com>
2020-05-13 14:54:31 +0200

    Revert "FAI-142 - avoid NaNs and Infinity in scores"
    This reverts commit 5101fe41380fbcb7f05fa4979c74cdc906841b48.

--- end2endPoC/explanation-service/src/main/java/com/redhat/developer/utils/LinearModel.java
+++ end2endPoC/explanation-service/src/main/java/com/redhat/developer/utils/LinearModel.java
@@ -1,7 +1,9 @@
 package com.redhat.developer.utils;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 import com.redhat.developer.model.Prediction;
@@ -14,7 +16,7 @@ public class LinearModel {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private final double[] weights;
+    private double[] weights;
     private final boolean classification;
     private final double threshold;
 
@@ -27,7 +29,7 @@ public class LinearModel {
         this.threshold = threshold;
         SecureRandom secureRandom = new SecureRandom();
         for (int i = 0; i < weights.length; i++) {
-            this.weights[i] = 1f / (1d + secureRandom.nextInt(100));
+            this.weights[i] = 1f / (double) secureRandom.nextInt(100);
         }
         this.classification = classification;
     }
@@ -35,8 +37,9 @@ public class LinearModel {
     public void fit(Collection<Prediction> trainingData) {
         assert !trainingData.isEmpty() : "cannot fit on an empty dataset";
 
-        double loss = 1;
+        double loss;
         for (int e = 0; e < 3; e++) {
+            int i = 0;
             for (Prediction prediction : trainingData) {
                 PredictionInput input = prediction.getInput();
                 PredictionOutput output = prediction.getOutput();
@@ -44,25 +47,18 @@ public class LinearModel {
                 double predictedOutput = predict(doubles);
                 double targetOutput = DataUtils.toNumbers(output)[0]; // assume the output has always one element (by previous label encoding construction)
                 double diff = targetOutput - predictedOutput;
-                diff = checkFinite(diff);
-                loss = Math.max(0, 1 - checkFinite(targetOutput * predictedOutput));
+                loss = Math.max(0, 1 - targetOutput * predictedOutput);
                 if (diff != 0) { // avoid null update to save computation
                     for (int j = 0; j < weights.length; j++) {
-                        double v = 1e-2 * diff * doubles[j];
-                        v = checkFinite(v);
-                        weights[j] += v;
+                        weights[j] += 1e-2 * diff * doubles[j];
                     }
                 }
+                i++;
+                if (i == trainingData.size()) {
+                    logger.info("loss: {}", loss);
+                }
             }
-            logger.info("loss: {}", loss);
         }
-    }
-
-    private double checkFinite(double diff) {
-        if (Double.isNaN(diff) || Double.isInfinite(diff)) {
-            diff = 0;
-        }
-        return diff;
     }
 
     private double predict(double[] input) {
