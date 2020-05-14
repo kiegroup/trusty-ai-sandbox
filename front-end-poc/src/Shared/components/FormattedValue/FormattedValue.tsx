392f87ea26dcea88ba5a95566e82c54ff416ca01
roberto emanuel <kelvah@gmail.com>
2020-05-14 16:10:43 +0200

    Adding breaking lines to formatted lists

--- front-end-poc/src/Shared/components/FormattedValue/FormattedValue.tsx
+++ front-end-poc/src/Shared/components/FormattedValue/FormattedValue.tsx
@@ -1,52 +1,55 @@
-import React from 'react';
-import './FormattedValue.scss';
+import React from "react";
+import { v4 as uuid } from "uuid";
+import "./FormattedValue.scss";
 
 const FormattedList = (props: { valueList: any[] }) => {
-    const { valueList } = props;
+  const { valueList } = props;
 
-    if (valueList.length === 0) {
-        return <span className="formatted-list formatted-list--no-entries">No entries</span>
-    }
-    return (
-        <span className="formatted-list">
-            {valueList.map((item, index) => (
-                <React.Fragment key={Math.floor(Math.random() * 10000)}>
-                    <FormattedValue value={item} key={Math.floor(Math.random() * 10000)} />
-                    {(index < valueList.length - 1) && <span key={Math.floor(Math.random() * 10000)}>, </span>}
-                </React.Fragment>
-            ))}
-        </span>
-    );
+  if (valueList.length === 0) {
+    return <span className="formatted-list formatted-list--no-entries">No entries</span>;
+  }
+  return (
+    <span className="formatted-list">
+      {valueList.map((item, index) => (
+        <React.Fragment key={uuid()}>
+          <FormattedValue value={item} key={uuid()} />
+          {index < valueList.length - 1 && (
+            <span key={uuid()}>
+              ,<br />
+            </span>
+          )}
+        </React.Fragment>
+      ))}
+    </span>
+  );
 };
 
 const FormattedValue = (props: { value: any }) => {
-    const { value } = props;
-    let formattedValue;
+  const { value } = props;
+  let formattedValue;
 
-    switch (typeof value) {
-        case 'number':
-        case 'bigint':
-            formattedValue = value;
-            break;
-        case 'string':
-            formattedValue = value;
-            break;
-        case 'boolean':
-            formattedValue = value.toString()
-            break;
-        case 'object':
-            if (Array.isArray(value)) {
-                formattedValue = <FormattedList valueList={value} />
-            }
-            break;
-        default:
-            formattedValue =  '';
-            break;
-    }
+  switch (typeof value) {
+    case "number":
+    case "bigint":
+      formattedValue = value;
+      break;
+    case "string":
+      formattedValue = value;
+      break;
+    case "boolean":
+      formattedValue = value.toString();
+      break;
+    case "object":
+      if (Array.isArray(value)) {
+        formattedValue = <FormattedList valueList={value} />;
+      }
+      break;
+    default:
+      formattedValue = "";
+      break;
+  }
 
-    return (
-        <span>{formattedValue}</span>
-    );
+  return <span>{formattedValue}</span>;
 };
 
 export default FormattedValue;
