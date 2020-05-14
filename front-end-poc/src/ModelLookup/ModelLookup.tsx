ccaf44a2253447e900c458d5698c86e28122190a
roberto emanuel <kelvah@gmail.com>
2020-05-14 14:16:37 +0200

    [FAI-141] Fixing model data retrieval

--- front-end-poc/src/ModelLookup/ModelLookup.tsx
+++ front-end-poc/src/ModelLookup/ModelLookup.tsx
@@ -1,5 +1,5 @@
 import { Divider, PageSection, PageSectionVariants, Spinner } from "@patternfly/react-core";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IExecutionModelResponse, IExecutionRouteParams } from "../Audit/types";
 import { getModelDetail } from "../Shared/api/audit.api";
@@ -7,36 +7,32 @@ import ModelDiagram from "./ModelDiagram";
 import "./ModelLookup.scss";
 
 const ModelLookup = () => {
-    const { executionId } = useParams<IExecutionRouteParams>();
-    const [viewer, setViewer] = useState(<Spinner size="xl" />);
+  const { executionId } = useParams<IExecutionRouteParams>();
+  const [viewer, setViewer] = useState(<Spinner size="xl" />);
 
-    useEffect(() => {
-        let didMount = true;
-        getModelDetail(executionId)
-            .then(response => {
-                if (didMount) {
-                    const model: IExecutionModelResponse = response.data[0] as IExecutionModelResponse;
-                    setViewer(<ModelDiagram model={model} />);
-                }
-            })
-            .catch(() => { });
-        return () => {
-            didMount = false;
-        };
-    }, [executionId]);
+  useEffect(() => {
+    let didMount = true;
+    getModelDetail(executionId)
+      .then((response) => {
+        if (didMount) {
+          const model: IExecutionModelResponse = response.data as IExecutionModelResponse;
+          setViewer(<ModelDiagram model={model} />);
+        }
+      })
+      .catch(() => {});
+    return () => {
+      didMount = false;
+    };
+  }, [executionId]);
 
-    return (
-        <>
-            <PageSection
-                variant={PageSectionVariants.light}
-                style={{ paddingTop: 0, paddingBottom: 0 }}>
-                <Divider />
-            </PageSection>
-            <PageSection variant={"light"}>
-                {viewer}
-            </PageSection>
-        </>
-    );
+  return (
+    <>
+      <PageSection variant={PageSectionVariants.light} style={{ paddingTop: 0, paddingBottom: 0 }}>
+        <Divider />
+      </PageSection>
+      <PageSection variant={"light"}>{viewer}</PageSection>
+    </>
+  );
 };
 
 export default ModelLookup;
