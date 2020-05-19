eedaa2ac542009435e037d8723c80a9e1ecb8edb
danielezonca <dzonca@redhat.com>
2020-05-19 10:46:44 +0200

    Fix script + re-enable kafdrop

--- end2endPoC/sample-requests/infinite-query-dmn-myMortgage.sh
+++ end2endPoC/sample-requests/infinite-query-dmn-myMortgage.sh
@@ -1,1 +1,1 @@
-while true; do (./random-query-dmn-risk.sh ; sleep $(bc -l <<< "scale=2 ; ${RANDOM}/6552")); done
+while true; do (./random-query-dmn-myMortgage.sh ; sleep $(bc -l <<< "scale=2 ; ${RANDOM}/6552")); done
