9792ea327109602c9b62b8bbf03d20a07c3942fa
Tommaso Teofili <tteofili@redhat.com>
2020-05-21 17:41:13 +0200

    FAI-146 - added explainability quantification unit test

---
+++ end2endPoC/explanation-service/src/test/java/com/redhat/developer/utils/ExplainabilityUtilsTest.java
@@ -0,0 +1,45 @@
+package com.redhat.developer.utils;
+
+import java.security.SecureRandom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExplainabilityUtilsTest {
+
+    private static final SecureRandom random = new SecureRandom();
+
+    @Test
+    public void testExplainabilityNoExplanation() {
+        double v = ExplainabilityUtils.quantifyExplainability(0, 0, 0);
+        assertFalse(Double.isNaN(v));
+        assertFalse(Double.isInfinite(v));
+        assertEquals(0, v);
+    }
+
+    @Test
+    public void testExplainabilityNoExplanationWithInteraction() {
+        double v = ExplainabilityUtils.quantifyExplainability(0, 0, 1);
+        assertFalse(Double.isNaN(v));
+        assertFalse(Double.isInfinite(v));
+        assertEquals(0, v);
+    }
+
+    @Test
+    public void testExplainabilityRandomchunksNoInteraction() {
+        double v = ExplainabilityUtils.quantifyExplainability(random.nextInt(), random.nextInt(), 1d /
+                random.nextInt(100));
+        assertFalse(Double.isNaN(v));
+        assertFalse(Double.isInfinite(v));
+        assertTrue(v >= 0 && v <= 1);
+    }
+
+    @Test
+    public void testExplainabilityRandom() {
+        double v = ExplainabilityUtils.quantifyExplainability(random.nextInt(), random.nextInt(), random.nextDouble());
+        assertFalse(Double.isNaN(v));
+        assertFalse(Double.isInfinite(v));
+        assertTrue(v >= 0 && v <= 1);
+    }
+}
