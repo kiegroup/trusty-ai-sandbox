a4a8a0e9143ba5766ae3c35c98136439f905ca3a
roberto emanuel <kelvah@gmail.com>
2020-05-26 16:16:15 +0200

    UI: updated mocks to open different outcomes

--- front-end-poc/api-mock/db.js
+++ front-end-poc/api-mock/db.js
@@ -1,51 +1,42 @@
-const faker = require('faker');
+const faker = require("faker");
 //const inputDataWithScores = require('./mocks/inputDataWithScores');
-const inputData = require('./mocks/inputData');
-const outcomeData = require('./mocks/outcomes');
-const outcomeDetailData = require('./mocks/outcomeDetail');
-const modelData = require('./mocks/modelData');
-const featureImportance = require('./mocks/featureImportance');
+const inputData = require("./mocks/inputData");
+const outcomeData = require("./mocks/outcomes");
+const outcomeDetailData = require("./mocks/outcomeDetail");
+const modelData = require("./mocks/modelData");
+const featureImportance = require("./mocks/featureImportance");
 
 let generateFakeAPIs = () => {
+  let decisionsList = [];
 
-    let decisionsArray = [];
+  for (let id = 1000; id < 1010; id++) {
+    let executionDate = faker.date.past();
 
-    for (let id = 1000; id < 1010; id++) {
-        let executionDate = faker.date.past();
+    decisionsList.push({
+      executionId: id,
+      executionDate: executionDate,
+      executionType: "DECISION",
+      executionSucceeded: true,
+      executorName: "Technical User",
+    });
+  }
 
-        decisionsArray.push({
-            "executionId": id,
-            "executionDate": executionDate,
-            "executionType": "DECISION",
-            "executionSucceeded": true,
-            "executorName": "Technical User"
-        });
-    }
+  let executionsList = {
+    total: 65,
+    limit: 10,
+    offset: 0,
+    headers: decisionsList,
+  };
 
-    let decisionsList = {
-        "total": 65,
-        "limit": 10,
-        "offset": 0,
-        "headers": decisionsArray
-    };
-
-    let decisionDetail = {
-        "executionId": faker.random.number(),
-        "executionDate": faker.date.past(),
-        "executionType": "DECISION",
-        "executionSucceeded": true,
-        "executorName": faker.name.findName()
-    }
-
-    return {
-        "executions": decisionsList,
-        "decisions": decisionDetail,
-        "inputs": inputData,
-        "outcomes": outcomeData,
-        "outcomeDetail": outcomeDetailData,
-        "models": modelData,
-        "featureImportance": featureImportance
-    }
+  return {
+    executions: executionsList,
+    decisions: decisionsList,
+    inputs: inputData,
+    outcomes: outcomeData,
+    outcomeDetail: outcomeDetailData,
+    models: modelData,
+    featureImportance: featureImportance,
+  };
 };
 
 module.exports = generateFakeAPIs;
