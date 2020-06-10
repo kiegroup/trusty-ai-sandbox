d6886fa17b293563ae2743da607e80dbb3fe5e8a
roberto emanuel <kelvah@gmail.com>
2020-06-10 18:36:03 +0200

    [FAI-192] Added various mocked examples of outcomes

--- front-end-poc/api-mock/mocks/modelData.js
+++ front-end-poc/api-mock/mocks/modelData.js
@@ -1,217 +1,220 @@
 // See http://dmg.org/pmml/v4-4/Regression.html#xsdElement_RegressionModel
-const example1 = "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
-    "<Header copyright='DMG.org'/>" +
-    "<DataDictionary numberOfFields='4'>" +
-    "  <DataField name='age' optype='continuous' dataType='double'/>" +
-    "  <DataField name='salary' optype='continuous' dataType='double'/>" +
-    "  <DataField name='car_location' optype='categorical' dataType='string'>" +
-    "    <Value value='carpark'/>" +
-    "    <Value value='street'/>" +
-    "    <Value value='garage'/>" +
-    "  </DataField>" +
-    "  <DataField name='number_of_claims' optype='continuous' dataType='integer'/>" +
-    "</DataDictionary>" +
-    "<RegressionModel modelName='Sample for linear regression' functionName='regression' algorithmName='linearRegression' targetFieldName='number_of_claims'>" +
-    "  <MiningSchema>" +
-    "    <MiningField name='age'/>" +
-    "    <MiningField name='salary'/>" +
-    "    <MiningField name='car_location'/>" +
-    "    <MiningField name='number_of_claims' usageType='target'/>" +
-    "  </MiningSchema>" +
-    "  <RegressionTable intercept='132.37'>" +
-    "    <NumericPredictor name='age' exponent='1' coefficient='7.1'/>" +
-    //  "    <NumericPredictor name='salary' exponent='1' coefficient='0.01'/>" +
-    "    <CategoricalPredictor name='car_location' value='carpark' coefficient='41.1'/>" +
-    "    <CategoricalPredictor name='car_location' value='street' coefficient='325.03'/>" +
-    "    <CategoricalPredictor name='car_location' value='garage' coefficient='-500.0'/>" +
-    "  </RegressionTable>" +
-    "</RegressionModel>" +
-    "</PMML>";
+const example1 =
+  "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
+  "<Header copyright='DMG.org'/>" +
+  "<DataDictionary numberOfFields='4'>" +
+  "  <DataField name='age' optype='continuous' dataType='double'/>" +
+  "  <DataField name='salary' optype='continuous' dataType='double'/>" +
+  "  <DataField name='car_location' optype='categorical' dataType='string'>" +
+  "    <Value value='carpark'/>" +
+  "    <Value value='street'/>" +
+  "    <Value value='garage'/>" +
+  "  </DataField>" +
+  "  <DataField name='number_of_claims' optype='continuous' dataType='integer'/>" +
+  "</DataDictionary>" +
+  "<RegressionModel modelName='Sample for linear regression' functionName='regression' algorithmName='linearRegression' targetFieldName='number_of_claims'>" +
+  "  <MiningSchema>" +
+  "    <MiningField name='age'/>" +
+  "    <MiningField name='salary'/>" +
+  "    <MiningField name='car_location'/>" +
+  "    <MiningField name='number_of_claims' usageType='target'/>" +
+  "  </MiningSchema>" +
+  "  <RegressionTable intercept='132.37'>" +
+  "    <NumericPredictor name='age' exponent='1' coefficient='7.1'/>" +
+  //  "    <NumericPredictor name='salary' exponent='1' coefficient='0.01'/>" +
+  "    <CategoricalPredictor name='car_location' value='carpark' coefficient='41.1'/>" +
+  "    <CategoricalPredictor name='car_location' value='street' coefficient='325.03'/>" +
+  "    <CategoricalPredictor name='car_location' value='garage' coefficient='-500.0'/>" +
+  "  </RegressionTable>" +
+  "</RegressionModel>" +
+  "</PMML>";
 
-const example2 = "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
-    "<Header copyright='DMG.org'/>" +
-    "<DataDictionary numberOfFields='2'>" +
-    "  <DataField name='water_temperature' optype='continuous' dataType='double'/>" +
-    "  <DataField name='hemisphere' optype='categorical' dataType='string'>" +
-    "    <Value value='northern'/>" +
-    "    <Value value='southern'/>" +
-    "  </DataField>" +
-    "  <DataField name='height_of_tide' optype='continuous' dataType='double'/>" +
-    "</DataDictionary>" +
-    "<RegressionModel modelName='Tide height' functionName='regression' algorithmName='linearRegression' targetFieldName='height_of_tide'>" +
-    "  <MiningSchema>" +
-    "    <MiningField name='water_temperature'/>" +
-    "    <MiningField name='height_of_tide' usageType='target'/>" +
-    "  </MiningSchema>" +
-    "  <RegressionTable intercept='1.2'>" +
-    "    <NumericPredictor name='water_temperature' exponent='1' coefficient='6'/>" +
-    "    <CategoricalPredictor name='hemisphere' value='northern' coefficient='-2.0'/>" +
-    "  </RegressionTable>" +
-    "</RegressionModel>" +
-    "</PMML>";
+const example2 =
+  "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
+  "<Header copyright='DMG.org'/>" +
+  "<DataDictionary numberOfFields='2'>" +
+  "  <DataField name='water_temperature' optype='continuous' dataType='double'/>" +
+  "  <DataField name='hemisphere' optype='categorical' dataType='string'>" +
+  "    <Value value='northern'/>" +
+  "    <Value value='southern'/>" +
+  "  </DataField>" +
+  "  <DataField name='height_of_tide' optype='continuous' dataType='double'/>" +
+  "</DataDictionary>" +
+  "<RegressionModel modelName='Tide height' functionName='regression' algorithmName='linearRegression' targetFieldName='height_of_tide'>" +
+  "  <MiningSchema>" +
+  "    <MiningField name='water_temperature'/>" +
+  "    <MiningField name='height_of_tide' usageType='target'/>" +
+  "  </MiningSchema>" +
+  "  <RegressionTable intercept='1.2'>" +
+  "    <NumericPredictor name='water_temperature' exponent='1' coefficient='6'/>" +
+  "    <CategoricalPredictor name='hemisphere' value='northern' coefficient='-2.0'/>" +
+  "  </RegressionTable>" +
+  "</RegressionModel>" +
+  "</PMML>";
 
-const example3 = "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
-    "<Header copyright='DMG.org'/>" +
-    "<DataDictionary numberOfFields='2'>" +
-    "  <DataField name='age' optype='continuous' dataType='double'>" +
-    "    <Interval closure='closedClosed' leftMargin='0' rightMargin='100'/>" +
-    "  </DataField>" +
-    "  <DataField name='weight' optype='continuous' dataType='double'>" +
-    "    <Interval closure='closedClosed' leftMargin='0' rightMargin='200'/>" +
-    "  </DataField>" +
-    "</DataDictionary>" +
-    "<RegressionModel modelName='You get fatter as you get older' functionName='regression' algorithmName='linearRegression' targetFieldName='height_of_tide'>" +
-    "  <MiningSchema>" +
-    "    <MiningField name='age'/>" +
-    "    <MiningField name='weight' usageType='target'/>" +
-    "  </MiningSchema>" +
-    "  <RegressionTable intercept='2'>" +
-    "    <NumericPredictor name='age' exponent='1' coefficient='1.6'/>" +
-    "  </RegressionTable>" +
-    "</RegressionModel>" +
-    "</PMML>";
+const example3 =
+  "<PMML xmlns='http://www.dmg.org/PMML-4_4' version='4.4'> " +
+  "<Header copyright='DMG.org'/>" +
+  "<DataDictionary numberOfFields='2'>" +
+  "  <DataField name='age' optype='continuous' dataType='double'>" +
+  "    <Interval closure='closedClosed' leftMargin='0' rightMargin='100'/>" +
+  "  </DataField>" +
+  "  <DataField name='weight' optype='continuous' dataType='double'>" +
+  "    <Interval closure='closedClosed' leftMargin='0' rightMargin='200'/>" +
+  "  </DataField>" +
+  "</DataDictionary>" +
+  "<RegressionModel modelName='You get fatter as you get older' functionName='regression' algorithmName='linearRegression' targetFieldName='height_of_tide'>" +
+  "  <MiningSchema>" +
+  "    <MiningField name='age'/>" +
+  "    <MiningField name='weight' usageType='target'/>" +
+  "  </MiningSchema>" +
+  "  <RegressionTable intercept='2'>" +
+  "    <NumericPredictor name='age' exponent='1' coefficient='1.6'/>" +
+  "  </RegressionTable>" +
+  "</RegressionModel>" +
+  "</PMML>";
 
 const modelData = [
-    {
-        executionId: 1000,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "myMortgage",
-        namespace: "modelNameSpace",
-        type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: ""
+  {
+    executionId: 1000,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "myMortgage",
+    namespace: "modelNameSpace",
+    type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1001,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "fraud-scoring",
-        namespace: "modelNameSpace",
-        type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: ""
+    model: "",
+  },
+  {
+    executionId: 1001,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1002,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: ""
+    model: "",
+  },
+  {
+    executionId: 1002,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1003,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: ""
+    model: "",
+  },
+  {
+    executionId: 1003,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1004,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: ""
+    model: "",
+  },
+  {
+    executionId: 1004,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.omg.org/spec/DMN/20151101/dmn.xsd",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1005,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.dmg.org/PMML-4_4",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: example1
+    model: "",
+  },
+  {
+    executionId: 1005,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.dmg.org/PMML-4_4",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1006,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.dmg.org/PMML-4_4",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: example2
+    model: example1,
+  },
+  {
+    executionId: 1006,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.dmg.org/PMML-4_4",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1007,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.dmg.org/PMML-4_4",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: example3
+    model: example2,
+  },
+  {
+    executionId: 1007,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.dmg.org/PMML-4_4",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1008,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.dmg.org/PMML-4_4",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: example1
+    model: example3,
+  },
+  {
+    executionId: 1008,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.dmg.org/PMML-4_4",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
     },
-    {
-        executionId: 1009,
-        deploymentDate: "01012020",
-        modelId: "1234567890",
-        name: "modelName",
-        namespace: "modelNameSpace",
-        type: "http://www.dmg.org/PMML-4_4",
-        serviceIdentifier: {
-            groupId: "groupId",
-            artifactId: "artifacrtId",
-            version: "version"
-        },
-        model: example2
-    }
+    model: example1,
+  },
+  {
+    executionId: 1009,
+    deploymentDate: "01012020",
+    modelId: "1234567890",
+    name: "fraud-scoring",
+    namespace: "modelNameSpace",
+    type: "http://www.dmg.org/PMML-4_4",
+    serviceIdentifier: {
+      groupId: "groupId",
+      artifactId: "artifacrtId",
+      version: "version",
+    },
+    model: example2,
+  },
 ];
 
 module.exports = modelData;
